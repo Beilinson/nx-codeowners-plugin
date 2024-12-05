f33f8925d67588e55065a8d13cbbd7f2329b46e4
Beilinson <adam.v.beili@gmail.com>
2024-12-05 20:20:06 +0200

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/4ggEKFu551

---
+++ nx-codeowners-plugin/README.md
@@ -0,0 +1,11 @@
+# nx-codeowners-plugin
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-codeowners-plugin` to build the library.
+
+## Running unit tests
+
+Run `nx test nx-codeowners-plugin` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ nx-codeowners-plugin/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'nx-codeowners-plugin',
+  preset: '../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/nx-codeowners-plugin',
+};
